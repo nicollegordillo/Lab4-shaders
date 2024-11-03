ccc518b0545b93288c78b66889b8b2eca4c1ba18
nicollegordillo <nicollegordillo912@gmail.com>
2024-11-03 15:16:22 -0600

    Feat: added basic files

---
+++ src/color.rs
@@ -0,0 +1,54 @@
+// color.rs
+use std::ops::{Add, Mul};
+use std::fmt;
+#[derive(Clone, Copy, Debug)]
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+impl Color {
+    pub fn new(r: f32, g: f32, b: f32) -> Self {
+        Color { r, g, b }
+    }
+
+    pub fn black() -> Self {
+        Color { r: 0.0, g: 0.0, b: 0.0 }
+    }
+
+    pub fn to_hex(&self) -> u32 {
+        ((self.r as u32) << 16) | ((self.g as u32) << 8) | (self.b as u32)
+      }
+
+}
+
+impl Mul<f32> for Color {
+    type Output = Color;
+
+    fn mul(self, scalar: f32) -> Color {
+        Color {
+            r: self.r * scalar,
+            g: self.g * scalar,
+            b: self.b * scalar,
+        }
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+        }
+    }
+}
+
+impl fmt::Display for Color {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "Color(r: {}, g: {}, b: {})", self.r, self.g, self.b)
+  }
+}
